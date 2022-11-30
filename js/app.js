21bc1d1043aefcfc54e4ee4b7b544c14a88cd5e5
Abbat <misochka-piva@yandex.ru>
2022-11-30 12:22:21 +0300

    done

--- js/app.js
+++ js/app.js
@@ -1,10 +1,33 @@
 $(function() {
 
+    /* Nav Toggle on mobile
+    ======================================================= */
+
+    let navToggle = $('#navToggle');
+    let nav = $('#nav');
+
+    navToggle.on('click', function(event) {
+        event.preventDefault();
+
+        $('body').toggleClass('show-nav');
+        $(this).toggleClass('active');
+        nav.toggleClass('show');
+    });
+
+
+
+
     let intro = $('#intro');
     let header = $('#header');
     let introH = intro.innerHeight();
     let headerH = header.innerHeight();
     let scrollTop = $(window).scrollTop();
+
+    $(window).on('resize', function() {
+        $('body').removeClass('show-nav');
+        navToggle.removeClass('active');
+        nav.removeClass('show');
+    });
 
 
     /* Header class on scroll
@@ -13,9 +36,7 @@ $(function() {
     headerScroll();
 
     $(window).on('scroll resize', function() {
-        
         headerScroll();
-
     });
 
 
@@ -43,6 +64,10 @@ $(function() {
 
         let scrollEl = $(this).data('scroll');
         let scrollElPos = $(scrollEl).offset().top;
+
+        $('body').toggleClass('show-nav');
+        navToggle.removeClass('active');
+        nav.removeClass('show');
 
         $('html, body').animate({
             scrollTop: scrollElPos - headerH
@@ -133,6 +158,64 @@ $(function() {
             modal.removeClass('show');
         }, 200);          
     }
+
+
+    /* Slick slider
+    ======================================================= */
+
+    /* Intro slider
+    ============================================ */
+
+    let introSlider = $('#introSlider');
+
+    introSlider.slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 4000
+    });
+
+    $('#introSliderPrev').on('click', function() {
+        introSlider.slick('slickPrev')
+    });
+
+    $('#introSliderNext').on('click', function() {
+        introSlider.slick('slickNext')
+    });
+
+
+
+    /* AOS
+    ============================================ */
+
+    AOS.init({
+      // Global settings:
+      disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+      startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+      initClassName: 'aos-init', // class applied after initialization
+      animatedClassName: 'aos-animate', // class applied on animation
+      useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+      disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+      debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+      throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+      
+    
+      // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+      offset: 120, // offset (in px) from the original trigger point
+      delay: 0, // values from 0 to 3000, with step 50ms
+      duration: 400, // values from 0 to 3000, with step 50ms
+      easing: 'ease', // default easing for AOS animations
+      once: false, // whether animation should happen only once - while scrolling down
+      mirror: false, // whether elements should animate out while scrolling past them
+      anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+    
+    });
+
+
+
 
 });
 
